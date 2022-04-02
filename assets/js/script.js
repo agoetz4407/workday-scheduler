9940b51569cf4fb3c49780f586e25169072c8cf5
Andrew Goetz <agoetz4407@gmail.com>
2022-04-02 16:56:02 -0500

    final functionality with loaded tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,35 @@
 //extending dayjs formatting options
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-
 //setting current day in header
 $("#currentDay").text(dayjs().format('dddd, MMMM Do'));
 
+//loading saved tasks function
+var loadTasks = function() {
+    var loadedTasks = localStorage.getItem("SavedTasks")
+    if (!loadedTasks) {
+        return;
+    }
+    loadedTasks = JSON.parse(loadedTasks)
+    for (var i = 0; i < loadedTasks.length; i++) {
+        var loadedEvent = loadedTasks[i].event;
+        var loadedTime = loadedTasks[i].time;
+        var currentEvent = $(`.description[data-time=${loadedTime}]`);
+        currentEvent.text(loadedEvent);
+    }
+}
 
+//on save button click function to save tasks
 $(".saveBtn").on("click", function() {
     var currentEvent = $(this).siblings(".description").val();
     var time = $(this).siblings(".description").attr("data-time");
     var savedTasks = localStorage.getItem("SavedTasks");
+    //if there are no saved tasks, create new local storage for tasks
     if (currentEvent && !savedTasks) {
         localStorage.setItem("SavedTasks", JSON.stringify([{time: time, event: currentEvent}]));
         return;
     }
+    // replace saved task with new task at selected time
     else if (currentEvent && savedTasks) {
         savedTasks = JSON.parse(savedTasks)
         //loop through array to remove task that are already assigned to time we are trying to save
@@ -25,8 +41,20 @@ $(".saveBtn").on("click", function() {
         savedTasks.push({time: time, event: currentEvent})
         localStorage.setItem("SavedTasks", JSON.stringify(savedTasks))
     }
+    //if current event is empty, remove saved task at that time
+    else if (!currentEvent && savedTasks) {
+        savedTasks = JSON.parse(savedTasks)
+        //loop through saved tasks to remove task at time
+        for (var i = 0; i < savedTasks.length; i++) {
+            if (time == savedTasks[i].time) {
+                savedTasks.splice(i, 1)
+            }
+        }
+        localStorage.setItem("SavedTasks", JSON.stringify(savedTasks))
+    }
 });
 
+//setting class to style time blocks correctly based on current time
 var setTimeClass = function() {
 
     currentTime = dayjs().hour();
@@ -50,7 +78,8 @@ var setTimeClass = function() {
 
 //checking time every minute to change time block status/color
 setInterval(setTimeClass, 60000);
-
+//initial check time and set time block status/color
 setTimeClass();
+loadTasks();
 
 
